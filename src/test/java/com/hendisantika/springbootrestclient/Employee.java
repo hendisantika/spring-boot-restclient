8a7d5aa0d92d202a79682646427903b009e2d1b9
hendisantika <hendisantika@yahoo.co.id>
2023-11-05 07:52:17 +0700

    #19 - Add Employee Model ✏️:octocat:🐧🐳

---
+++ src/test/java/com/hendisantika/springbootrestclient/Employee.java
@@ -0,0 +1,20 @@
+package com.hendisantika.springbootrestclient;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-restclient
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/5/23
+ * Time: 07:51
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class Employee {
+    private long id;
+    private String name;
+    private String status;
+}
