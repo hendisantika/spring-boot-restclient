2c7fdcba0e58f1dc653e53044ad3d7ae2c5320e5
hendisantika <hendisantika@yahoo.co.id>
2023-11-05 07:35:42 +0700

    #1 - Initial starter project ✏️:octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/springbootrestclient/SpringBootRestclientApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestclientApplication.class, args);
+    }
+
+}
