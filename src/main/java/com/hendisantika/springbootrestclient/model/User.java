48877056d3f2775a3039b553388558270e89bb47
hendisantika <hendisantika@yahoo.co.id>
2023-11-25 06:26:24 +0700

    #24 - Add User Record  🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springbootrestclient/model/User.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootrestclient.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-restclient
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/25/23
+ * Time: 06:25
+ * To change this template use File | Settings | File Templates.
+ */
+public record User(String id, String name, String address) {
+}
