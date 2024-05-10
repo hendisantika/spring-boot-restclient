26fc633058dd948cd98daa7c4faf67df2c0de38e
hendisantika <hendisantika@yahoo.co.id>
2024-05-10 18:01:49 +0700

    #39 - Add EmployeeJdbcRepository🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/springbootrestclient/repository/EmployeeJdbcRepository.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springbootrestclient.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-restclient
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/10/24
+ * Time: 18:01
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class EmployeeJdbcRepository {
+
+    private final JdbcClient jdbcClient;
+}
