be4c0a9b8e1c194532c94bb7e67d70654145bf84
hendisantika <hendisantika@yahoo.co.id>
2024-05-10 17:28:28 +0700

    #37 - Add Employee2 Model 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/springbootrestclient/model/Employee2.java
@@ -0,0 +1,44 @@
+package com.hendisantika.springbootrestclient.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-restclient
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/10/24
+ * Time: 17:26
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee2 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
