a0f79f653f0c399e3d4bbc4c2bf2a590f8bbb047
hendisantika <hendisantika@yahoo.co.id>
2024-05-10 18:12:01 +0700

    #61 - Add createEmployee in Employee2Controller🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/springbootrestclient/controller/Employee2Controller.java
+++ src/main/java/com/hendisantika/springbootrestclient/controller/Employee2Controller.java
@@ -1,7 +1,12 @@
 package com.hendisantika.springbootrestclient.controller;
 
+import com.hendisantika.springbootrestclient.dto.EmployeeDto;
 import com.hendisantika.springbootrestclient.service.EmployeeService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +26,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class Employee2Controller {
 
     private EmployeeService employeeService;
+
+    // build create employee REST API
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employee) {
+        EmployeeDto savedEmployee = employeeService.createEmployee(employee);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
 }
