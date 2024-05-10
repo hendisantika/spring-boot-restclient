69e71bb0aade8438c47b7e6285d92c9732f51421
hendisantika <hendisantika@yahoo.co.id>
2024-05-10 18:14:53 +0700

    #71 - Add deleteUser in User2Controller🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/springbootrestclient/controller/User2Controller.java
+++ src/main/java/com/hendisantika/springbootrestclient/controller/User2Controller.java
@@ -4,6 +4,7 @@ import com.hendisantika.springbootrestclient.model.User2;
 import com.hendisantika.springbootrestclient.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,5 +53,11 @@ public class User2Controller {
     @PutMapping("/{id}")
     public ResponseEntity<User2> updateUser(@PathVariable Long id, @RequestBody User2 user) {
         return ResponseEntity.ok(userService.saveUser(user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
