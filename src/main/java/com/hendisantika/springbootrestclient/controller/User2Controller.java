a73c6283bcd0396294fbad65a43b5096be783719
hendisantika <hendisantika@yahoo.co.id>
2024-05-10 18:14:31 +0700

    #70 - Add updateUser in User2Controller🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/springbootrestclient/controller/User2Controller.java
+++ src/main/java/com/hendisantika/springbootrestclient/controller/User2Controller.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +46,11 @@ public class User2Controller {
 
     @PostMapping
     public ResponseEntity<User2> createUser(@RequestBody User2 user) {
+        return ResponseEntity.ok(userService.saveUser(user));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User2> updateUser(@PathVariable Long id, @RequestBody User2 user) {
         return ResponseEntity.ok(userService.saveUser(user));
     }
 }
