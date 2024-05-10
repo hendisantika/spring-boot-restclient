184542e15ab491984b29a313d7e2c68aeb3cfecd
hendisantika <hendisantika@yahoo.co.id>
2024-05-10 18:12:41 +0700

    #64 - Add updateEmployee in Employee2Controller🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/springbootrestclient/controller/Employee2Controller.java
+++ src/main/java/com/hendisantika/springbootrestclient/controller/Employee2Controller.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,5 +53,15 @@ public class Employee2Controller {
     public ResponseEntity<List<EmployeeDto>> getAllEmployees() {
         List<EmployeeDto> employees = employeeService.getAllEmployees();
         return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    // build update employee REST API
+    // http://localhost:8080/api/employees/1
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") long id
+            , @RequestBody EmployeeDto employeeDto) {
+        employeeDto.setId(id);
+        EmployeeDto updatedEmployee = employeeService.updateEmployee(employeeDto);
+        return new ResponseEntity<EmployeeDto>(updatedEmployee, HttpStatus.OK);
     }
 }
