aca0d4cde0a99fd69e640d62d3e1a59935df9934
hendisantika <hendisantika@yahoo.co.id>
2024-05-10 18:14:18 +0700

    #69 - Add createUser in User2Controller🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/springbootrestclient/controller/User2Controller.java
+++ src/main/java/com/hendisantika/springbootrestclient/controller/User2Controller.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +41,10 @@ public class User2Controller {
         return userService.getUserById(id)
                 .map(entity -> ResponseEntity.ok(entity))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<User2> createUser(@RequestBody User2 user) {
+        return ResponseEntity.ok(userService.saveUser(user));
     }
 }
